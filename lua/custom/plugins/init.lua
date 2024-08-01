99b5364b9dedc5beec5031dbae91e2bf22074825
stijnlemm <lemmstijn@hotmail.com>
2024-08-01 20:38:15 +0200

    [init]

---
+++ lua/custom/plugins/init.lua
@@ -0,0 +1,15 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  'kdheepak/lazygit.nvim',
+  { 'bluz71/vim-moonfly-colors', name = 'moonfly', lazy = false, priority = 1000 },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = true,
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+  },
+}
