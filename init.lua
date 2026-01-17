ca138f2955d56b59f644dff2046abccfbfd1d879
stijnlemm <lemmstijn@hotmail.com>
2026-01-17 11:35:53 +0100

    [fix] clangd being mad

--- init.lua
+++ init.lua
@@ -522,6 +522,8 @@ require('lazy').setup({
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
+      capabilities.offsetEncoding = 'utf-8'
+
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --
